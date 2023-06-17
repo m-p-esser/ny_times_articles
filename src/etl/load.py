1f05fc0c5fc34fe68d97e651414dbb9a152aaa08
Marc-Philipp Esser <m-esser@mail.de>
2023-06-17 18:58:30 +0200

    add function to load file from blob (Google Cloud Storage

--- src/etl/load.py
+++ src/etl/load.py
@@ -28,3 +28,25 @@ def upload_blob_from_memory(
     # )
 
     return blob
+
+
+def upload_blob_from_file(
+    bucket_name, source_file_name, destination_blob_name
+) -> storage.bucket.Bucket.blob:
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    # print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+    return blob
