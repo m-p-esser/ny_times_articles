5f22e672482884046c14bc1ca915eab6d2b0d91a
Marc-Philipp Esser <m-esser@mail.de>
2023-06-17 18:57:49 +0200

    add function to download blob from GCS into memory

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -3,7 +3,7 @@
 from google.cloud import storage
 
 
-def download_blob(
+def download_blob_to_file(
     bucket_name, source_blob_name, destination_file_name
 ) -> storage.bucket.Bucket.blob:
     """Downloads a blob from the bucket."""
@@ -34,3 +34,30 @@ def download_blob(
     # )
 
     return blob
+
+
+def download_blob_into_memory(bucket_name, blob_name) -> str:
+    """Downloads a blob into memory."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(blob_name)
+    contents = blob.download_as_string()
+
+    # print(
+    #     "Downloaded storage object {} from bucket {} as the following string: {}.".format(
+    #         blob_name, bucket_name, contents
+    #     )
+    # )
+    return contents
